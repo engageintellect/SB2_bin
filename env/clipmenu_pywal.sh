89a833fc3f799f71d21176ae63256bf6bc312a76
jc9361 <jc9361@gmail.com>
2020-11-09 23:35:41 -0800

    updating...

--- env/clipmenu_pywal.sh
+++ env/clipmenu_pywal.sh
@@ -3,4 +3,5 @@
 # Import the colors
 . "${HOME}/.cache/wal/colors.sh"
 
-clipmenu -fn 'Hack Nerd Font -10' -nb "$color0" -nf "$color15" -sb "$color3" -sf "$color0" -h 60
+clipmenu -fn 'Hack Nerd Font -10' -nb "$color0" -nf "$color15" -sb "$color3" -sf "$color0" -h 60 -p " "
+
--- env/dmenu_pywal.sh
+++ env/dmenu_pywal.sh
@@ -3,4 +3,4 @@
 # Import the colors
 . "${HOME}/.cache/wal/colors.sh"
 
-dmenu_run -fn 'Hack Nerd Font -12' -nb "$color0" -nf "$color15" -sb "$color3" -sf "$color0" -h 60
+dmenu_run -fn 'Hack Nerd Font -10' -nb "$color0" -nf "$color15" -sb "$color1" -sf "$color15" -h 60 -p " " 
